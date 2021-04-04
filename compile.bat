76980955d918724db34bb1c483eda2ba4ccad2c7
Gray Programmerz <hackrefisher@gmail.com>
2021-04-04 13:15:23 +0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 7z.exe x vb6.7z -y
-"C:\Windows\system32\regedit.exe" /s register.reg
+regedit.exe /s register.reg
 set "dir=%cd%"
 mkdir output
 "vb6/VB6.exe" /MAKE "sample/Project1.vbp" /outdir "output"
