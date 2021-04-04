d79c01fae1cd72ea6f1699c51df01d825d867887
Gray Programmerz <hackrefisher@gmail.com>
2021-04-04 15:10:45 +0500

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,8 @@
+7z.exe x vb6_builder.7z -y
+7z.exe x vb6_runtime.7z -oC:\Windows\System32 -y
+reg add HKEY_CLASSES_ROOT\Licenses\6000720D-F342-11D1-AF65-00A0C90DCA10 /d "kefeflhlhlgenelerfleheietfmflelljeqf" /f
+reg add HKEY_CLASSES_ROOT\Licenses\74872840-703A-11d1-A3AF-00A0C90F26FA /d "mninuglgknogtgjnthmnggjgsmrmgniglish" /f
+reg add HKEY_CLASSES_ROOT\Licenses\74872841-703A-11d1-A3AF-00A0C90F26FA /d "klglsejeilmereglrfkleeheqkpkelgejgqf" /f
+set "dir=%cd%"
+mkdir output
+"vb6/VB6.exe" /MAKE "sample/Project1.vbp" /outdir "output"
